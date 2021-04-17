8f7bbe46999463508ffd5d2e62a8fee82b8b17ac
Ahmed Rasidun Bari Dip <67531074+AhmedDip@users.noreply.github.com>
2021-04-17 10:49:50 +0600

    Update uploadpp.php

--- uploadpp.php
+++ uploadpp.php
@@ -13,20 +13,20 @@
 
 
 <?php
-$target_dir = "image/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+ $target_dir = "image/";
+ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+ $uploadOk = 1;
+ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
+   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+      echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
   } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
+     echo "File is not an image.";
+      $uploadOk = 0;
   }
 }
 
@@ -64,8 +64,6 @@ if ($uploadOk == 0) {
 
 <section>
   <?php 
-
-
 
 if (isset($_SESSION['username'])) {
   echo "  
